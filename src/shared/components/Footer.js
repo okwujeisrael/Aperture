355b0a04d8fbbef2ab03a56e971bece5ef4e8e8e
Israel <okwujeisrael@yahoo.com>
2020-12-29 19:10:58 +0000

    Refactor to use Amazon s3

--- src/shared/components/Footer.js
+++ src/shared/components/Footer.js
@@ -5,21 +5,15 @@ const Footer = () => {
   return (
     <footer className="bg-purple-400 text-white text-center p-16 dosis-normal">
       <div>
-        <span className="inline-block w-8 h-8 rounded-full m-2 border-2 border-white hover:text-black">
-          <a href="http://facebook.com" target="_blank" rel="noreferrer">
-            <i className="fab fa-facebook-f"></i>
-          </a>
-        </span>
-        <span className="inline-block w-8 h-8 rounded-full m-2 border-2 border-white hover:text-black">
-          <a href="http://twitter.com" target="_blank" rel="noreferrer">
-            <i className="fab fa-twitter"></i>
-          </a>
-        </span>
-        <span className="inline-block w-8 h-8 rounded-full m-2 border-2 border-white hover:text-black">
-          <a href="http://linkedin.com" target="_blank" rel="noreferrer">
-            <i className="fab fa-linkedin-in"></i>
-          </a>
-        </span>
+        <a href="http://facebook.com" target="_blank" rel="noreferrer">
+          <i className="fab fa-facebook-f m-2 hover:text-black"></i>
+        </a>
+        <a href="http://twitter.com" target="_blank" rel="noreferrer">
+          <i className="fab fa-twitter m-2 hover:text-black"></i>
+        </a>
+        <a href="http://linkedin.com" target="_blank" rel="noreferrer">
+          <i className="fab fa-linkedin-in m-2 hover:text-black"></i>
+        </a>
       </div>
       <div className="tracking-widest">
         &copy; 2020 Aperture. All rights reserved
--- src/shared/components/Photogrid.js
+++ src/shared/components/Photogrid.js
@@ -1,69 +1,119 @@
-import React from 'react';
-
-import one from '../../Assets/homepage/one.jpg';
-import two from '../../Assets/homepage/two.jpg';
-import three from '../../Assets/homepage/three.jpg';
-import four from '../../Assets/homepage/four.jpg';
-import five from '../../Assets/homepage/five.jpg';
-import six from '../../Assets/homepage/six.jpg';
-import seven from '../../Assets/homepage/seven.jpg';
-import eight from '../../Assets/homepage/eight.jpg';
-import nine from '../../Assets/homepage/nine.jpg';
-import ten from '../../Assets/homepage/ten.jpg';
-
-
+import React from "react";
 
 const Photogrid = ({ handleClick }) => {
+  const imgBaseUrl =
+    "https://aperture2020.s3.eu-west-2.amazonaws.com/homepage/";
+
   return (
     <div className="w-screen h-1/2 flex flex-col md:flex-row space-y-4 md:space-y-0 space-x-0 md:space-x-2 px-4 pt-16 pb-20 bg-purple-400">
       <div className="md:w-1/3 h-96 flex space-x-2">
         <div className="w-1/2 h-full space-y-2">
           <div className="w-full h-1/2 border-8 border-black hover:border-white">
-            <img className="w-full h-full object-cover gray-img" src={one} alt="one" name="1" onClick={handleClick} />
+            <img
+              className="w-full h-full object-cover gray-img"
+              src={imgBaseUrl + "one.jpg"}
+              alt="one"
+              name="1"
+              onClick={handleClick}
+            />
           </div>
           <div className="w-full h-1/2 border-8 border-black hover:border-white">
-            <img className="w-full h-full object-cover gray-img" src={two} alt="two"  name="2" onClick={handleClick} />
+            <img
+              className="w-full h-full object-cover gray-img"
+              src={imgBaseUrl + "two.jpg"}
+              alt="two"
+              name="2"
+              onClick={handleClick}
+            />
           </div>
         </div>
         <div className="w-1/2 h-full border-8 border-black hover:border-white">
-          <img className="w-full h-full object-cover gray-img" src={three} alt="three" name="3" onClick={handleClick} />
+          <img
+            className="w-full h-full object-cover gray-img"
+            src={imgBaseUrl + "three.jpg"}
+            alt="three"
+            name="3"
+            onClick={handleClick}
+          />
         </div>
       </div>
 
       <div className="md:w-1/3 h-96 flex space-x-2">
         <div className="w-1/2 h-full space-y-2">
           <div className="w-full h-1/2 border-8 border-black hover:border-white">
-            <img className="w-full h-full object-cover gray-img" src={four} alt="four" name="4" onClick={handleClick} />
+            <img
+              className="w-full h-full object-cover gray-img"
+              src={imgBaseUrl + "four.jpg"}
+              alt="four"
+              name="4"
+              onClick={handleClick}
+            />
           </div>
           <div className="w-full h-1/2 border-8 border-black hover:border-white">
-            <img className="w-full h-full object-cover gray-img" src={five} alt="five" name="5" onClick={handleClick} />
+            <img
+              className="w-full h-full object-cover gray-img"
+              src={imgBaseUrl + "five.jpg"}
+              alt="five"
+              name="5"
+              onClick={handleClick}
+            />
           </div>
         </div>
         <div className="w-1/2 h-full space-y-2">
           <div className="w-full h-1/2 border-8 border-black hover:border-white">
-            <img className="w-full h-full object-cover gray-img" src={six} alt="six" name="6" onClick={handleClick} />
+            <img
+              className="w-full h-full object-cover gray-img"
+              src={imgBaseUrl + "six.jpg"}
+              alt="six"
+              name="6"
+              onClick={handleClick}
+            />
           </div>
           <div className="w-full h-1/2 border-8 border-black hover:border-white">
-            <img className="w-full h-full object-cover gray-img" src={seven} alt="seven" name="7" onClick={handleClick} />
+            <img
+              className="w-full h-full object-cover gray-img"
+              src={imgBaseUrl + "seven.jpg"}
+              alt="seven"
+              name="7"
+              onClick={handleClick}
+            />
           </div>
         </div>
       </div>
 
       <div className="md:w-1/3 h-96 flex space-x-2">
         <div className="w-1/2 h-full border-8 border-black hover:border-white">
-          <img className="w-full h-full object-cover gray-img" src={eight} alt="eight" name="8" onClick={handleClick} />
+          <img
+            className="w-full h-full object-cover gray-img"
+            src={imgBaseUrl + "eight.jpg"}
+            alt="eight"
+            name="8"
+            onClick={handleClick}
+          />
         </div>
         <div className="w-1/2 h-full space-y-2">
           <div className="w-full h-1/2 border-8 border-black hover:border-white">
-            <img className="w-full h-full object-cover gray-img" src={nine} alt="nine" name="9" onClick={handleClick} />
+            <img
+              className="w-full h-full object-cover gray-img"
+              src={imgBaseUrl + "nine.jpg"}
+              alt="nine"
+              name="9"
+              onClick={handleClick}
+            />
           </div>
           <div className="w-full h-1/2 border-8 border-black hover:border-white">
-            <img className="w-full h-full object-cover gray-img" src={ten} alt="ten" name="10" onClick={handleClick} />
+            <img
+              className="w-full h-full object-cover gray-img"
+              src={imgBaseUrl + "ten.jpg"}
+              alt="ten"
+              name="10"
+              onClick={handleClick}
+            />
           </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Photogrid;
